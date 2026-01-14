78ebd6fb2198b1bd91c8560c445e4fe828150a08
Do66i <xoxo.madlife@gmail.com>
2026-01-15 08:28:06 +0900

    refactor: UpdateBoardDto에 한국어 커스텀 에러 메시지 적용

--- src/boards/dto/update-board.dto.ts
+++ src/boards/dto/update-board.dto.ts
@@ -1,11 +1,10 @@
 import { IsOptional, IsString, MinLength } from 'class-validator';
 
-export class UpdateBoardDto{
+export class UpdateBoardDto {
     @IsString() // 문자열이어야 함
-    @MinLength(4) // 최소 길이 4
+    @MinLength(2, { message: '제목은 최소 2글자 이상 입력해야 합니다. ✍️' }) // 최소 길이 2
     title?: string;
 
-    @IsOptional()
     @IsString()
     description?: string;
 }
